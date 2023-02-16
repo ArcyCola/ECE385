6bf731f74bc7e8d849d5491882989a0d47da645a
ArcyCola <arcycruz14@gmail.com>
2023-02-15 22:09:11 -0600

    figuring out fsm

--- lab4/control.sv
+++ lab4/control.sv
@@ -1,5 +1,5 @@
-module control (input logic Clk, Run, Reset_Load_Clear
-                output logic Clr_Ld, Shift, Add, Sub )  
+module control (input logic Clk, Run, Reset_Load_Clear, M,
+                output logic Clr_Ld, Shift, fn )  
         //module for the FSM of the multiplier
     enum logic [3:0] {A, B, C, D, E, F, G, H, I, J} curr_state, next_state;
     
@@ -12,5 +12,53 @@ module control (input logic Clk, Run, Reset_Load_Clear
             curr_state <= next_state;
     end
 
+    always_comb
+    begin
 
+        next_state  = curr_state;	//required because I haven't enumerated all possibilities below
+        unique case (curr_state) 
+
+            A :    if (Execute)
+                        next_state = B;
+            B :      next_state = C;
+            C :      next_state = D;
+            D :      next_state = E;
+            E :      next_state = F;
+            F : 	 next_state = G;		// added for the 4 additional states
+            G : 	 next_state = H;
+            H : 	 next_state = I;
+            I : 	 next_state = J;
+            J :    if (~Execute) 
+                    next_state = A;
+                            
+        endcase
+        //assign outputs based on state
+        case (curr_state)   
+            A: 
+                begin  
+                    Clr_Ld = Reset_Load_Clear;
+                    Shift = 1'b0;
+                end
+            J: 
+                begin
+                    if (M)
+                        fn = 1'b1;
+                    else   
+                        fn = 1'b0;
+                end
+            default: //default case 
+                begin
+                        fn = 1'b0;
+                        Clr_Ld = 1'b0;
+
+
+
+                end
+        endcase
+
+    end 
 endmodule
+
+
+
+    
--- lab4/ripple_adder_9.sv
+++ lab4/ripple_adder_9.sv
@@ -1,6 +1,6 @@
 module ripple_adder_9
 (
-	input  [8:0] A, B,
+	input  [7:0] A, B,
 	input         fn,		// function select, decides if we add or subtract
 	output [8:0] S
 );
@@ -10,16 +10,25 @@ module ripple_adder_9
      * Insert code here to implement a ripple adder.
      * Your code should be completly combinational (don't use always_ff or always_latch).
      * Feel free to create sub-modules or other files. */
-	logic c1, c2, c3, c4, c5, c6, c7, c8;
+	logic c1, c2, c3, c4, c5, c6, c7, c8; 
+	logic [7:0] B_fn;
 	
-	full_adder FA0(.x(A[0]), .y(B[0]), .z(fn), .s(S[0]));
-	full_adder FA1(.x(A[1]), .y(B[1]), .z(fn), .s(S[1]));
-	full_adder FA2(.x(A[2]), .y(B[2]), .z(fn), .s(S[2]));
-	full_adder FA3(.x(A[3]), .y(B[3]), .z(fn), .s(S[3]));
-	full_adder FA4(.x(A[4]), .y(B[4]), .z(fn), .s(S[4]));
-	full_adder FA5(.x(A[5]), .y(B[5]), .z(fn), .s(S[5]));
-	full_adder FA6(.x(A[6]), .y(B[6]), .z(fn), .s(S[6]));
-	full_adder FA7(.x(A[7]), .y(B[7]), .z(fn), .s(S[7]));
-	full_adder FA8(.x(A[8]), .y(B[8]), .z(fn), .s(S[8]));
-	
+	always_comb
+	begin 
+		unique case (fn)
+			1'b1 : B_fn = B ^ fn; //fn = 1
+			1'b0 : B_fn = B; //for fn = 0
+		endcase
+	end 
+
+	full_adder FA0(.x(A[0]), .y(B_fn[0]), .z(0), .s(S[0]), .c(c1));
+	full_adder FA1(.x(A[1]), .y(B_fn[1]), .z(c1), .s(S[1]), .c(c2));
+	full_adder FA2(.x(A[2]), .y(B_fn[2]), .z(c2), .s(S[2]), .c(c3));
+	full_adder FA3(.x(A[3]), .y(B_fn[3]), .z(c3), .s(S[3]), .c(c4));
+	full_adder FA4(.x(A[4]), .y(B_fn[4]), .z(c4), .s(S[4]), .c(c5));
+	full_adder FA5(.x(A[5]), .y(B_fn[5]), .z(c5), .s(S[5]), .c(c6));
+	full_adder FA6(.x(A[6]), .y(B_fn[6]), .z(c6), .s(S[6]), .c(c7));
+	full_adder FA7(.x(A[7]), .y(B_fn[7]), .z(c7), .s(S[7]), .c(c8));
+	full_adder FA8(.x(A[7]), .y(B_fn[7]), .z(c8), .s(S[8]), .c());
+
 endmodule
