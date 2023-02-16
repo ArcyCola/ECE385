d2ecaf08ec08dfd6a1e8a3b44e0e278ecc7fdabb
ArcyCola <arcycruz14@gmail.com>
2023-02-15 20:19:05 -0600

    created new Processor for top-level, control for FSM

---
+++ lab4/control.sv
@@ -0,0 +1,4 @@
+module control (input logic Clk, Run, Reset_Load_Clear
+                output logic Clr_Ld, Shift, Add, Sub ) 
+
+endmodule
--- lab4/control_8_bit_multiplier.sv
+++
@@ -1,7 +0,0 @@
-module control_8_bit_multiplier (   input logic [7:0] SW,
-                                    input logic Clk, Reset_Load_Clear, Run,
-                                    output logic [6:0]  HEX0, HEX1, HEX2, HEX3,
-                                    output logic [7:0] Aval, Bval,
-                                    output logic Xval       ) 
-
-endmodule
