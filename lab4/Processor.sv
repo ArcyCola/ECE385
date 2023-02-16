e9b1ef4a71f8bd95cbfbba6fe2487da875edfab3
ArcyCola <arcycruz14@gmail.com>
2023-02-15 20:48:08 -0600

    just some edits

--- lab4/Processor.sv
+++ lab4/Processor.sv
@@ -3,5 +3,7 @@ module Processor (  input logic [7:0] SW,
                     output logic [6:0]  HEX0, HEX1, HEX2, HEX3,
                     output logic [7:0] Aval, Bval,
                     output logic Xval       ) 
+//top level unit for this lab
+
 
 endmodule
--- lab4/control.sv
+++ lab4/control.sv
@@ -1,4 +1,16 @@
 module control (input logic Clk, Run, Reset_Load_Clear
-                output logic Clr_Ld, Shift, Add, Sub ) 
+                output logic Clr_Ld, Shift, Add, Sub )  
+        //module for the FSM of the multiplier
+    enum logic [3:0] {A, B, C, D, E, F, G, H, I, J} curr_state, next_state;
+    
+    //copied from lab 2.2 vv
+    always_ff @ (posedge Clk)
+    begin
+        if (Reset)
+            curr_state <= A;
+        else 
+            curr_state <= next_state;
+    end
+
 
 endmodule
