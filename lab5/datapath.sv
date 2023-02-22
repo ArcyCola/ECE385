0b782da1ff384f5845e61d3f035e4bdbbee289e8
ArcyCola <arcycruz14@gmail.com>
2023-02-21 20:06:43 -0600

    starting MDR_MAR submodule

--- lab5/datapath.sv
+++ lab5/datapath.sv
@@ -1,6 +1,7 @@
 module datapath(input gatePC, gateMARMUX, gateALU, gateMDR, Clk,
-                output logic [15:0] MDR, PC, MARMUX, ALU); 
-    
+                output logic [15:0] databus); 
+    assign logic [15:0] MDR, PC, MARMUX, ALU, databus; //to be assigned to databus output w mux
+    //IR register will be instatinated here because needs that 4:1 MUX output
 endmodule
 
 module PCmod(input [15:0] PCadder, inDP, 
@@ -10,8 +11,9 @@ module PCmod(input [15:0] PCadder, inDP,
 
     logic [15:0] PC_next;
     //remember to connect reset to something, not implemented just yet
-    reg_16 PC(.Clk(Clk), .Reset() .Load(loadPC), .D(PC_next), .Data_Out(PC));
-    always_comb
+    reg_16 PCreg(.Clk(Clk), .Reset() .Load(loadPC), .D(PC_next), .Data_Out(PC));
+
+    always_comb //PCMUX implementation, 3?:1 MUX that goes back into PCreg
     begin  
         unique case (PCMUXsel)
             2'b00  :   PC_next <= PC + 1; //not sure if either <= or =, check if we can do " + 1"
@@ -23,4 +25,8 @@ module PCmod(input [15:0] PCadder, inDP,
     end
 endmodule
 
-module 
+module MAR_MDR_mod(input Clk, loadMAR, loadMDR, loadIR, MIO_EN,
+                 input [15:0] databus, Data_to_CPU, 
+                 output [15:0] MAR, MDR);
+        
+endmodule
