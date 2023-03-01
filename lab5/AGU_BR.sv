bad88a50564d182042f3ad3c1a75f5a5f8035c28
ArcyCola <arcycruz14@gmail.com>
2023-03-01 15:57:55 -0600

    implemented branch

--- lab5/AGU_BR.sv
+++ lab5/AGU_BR.sv
@@ -21,7 +21,6 @@ begin
     endcase
 end
 
-
 mux2_1  ADDR1_MUX(.In0(PC), .In1(SR1_OUT), .s(ADDR1MUX), .Out(ADDR1MUXout));
 
 mux4_1  ADDR2_MUX(.In0(16'b0), .In1(IRimm6sext), .In2(IRimm9sext), .In3(IRimm11sext), .s(ADDR2MUX), .Out(ADDR2MUXout));
@@ -30,9 +29,34 @@ mux4_1  ADDR2_MUX(.In0(16'b0), .In1(IRimm6sext), .In2(IRimm9sext), .In3(IRimm11s
 assign AGU_OUT = ADDR1MUXout + ADDR2MUXout;
 endmodule
 
+//change states in BEN is 1 or 0
+module BR(  input   [15:0] IR, D, 
+            input   LD_BEN, LD_CC, Clk,
+            output  BEN_Out);
 
-module BR(  input   [15:0] IR, D,
-            input   LD_BEN, LD_CC
-            output  );
+logic [2:0] nzpIn, nzpOut; 
+logic BEN_In, BEN_Out;
+
+always_comb 
+begin
+    if (D[15]) begin //negative
+        nzp = 3'b100;
+    end
+    else if (D == 16'b0) begin //zero
+        nzp = 3'b010;
+    end
+    else begin //positive
+        nzp = 3'b001;
+    end
+end
+    //idk what to put reset yet
+reg_3 NZP(.Clk, .Reset(), .Load(LD_CC), .D(nzp), .DataOut(nzpOut));
+ 
+//logic for S_32
+//BEN <- (IR[11] & N + IR[10] & Z + IR[9] & P)
+assign BEN_In = (IR[11] & nzp[2]) + (IR[10] & nzp[1]) + (IR[9] & nzp[0]);
+
+reg_1 BEN_reg(.Clk .Reset(), .Load(LD_BEN), .D(BEN_In) .DataOut(BEN_Out)); 
+//havent put logic for BEN, its the logic in state 32.
 
 endmodule
--- lab5/reg_16.sv
+++ lab5/reg_16.sv
@@ -14,3 +14,37 @@ module reg_16 ( input				 Clk, Reset, Load, DR, SR1, LD_REG
 		end
 		
 endmodule
+
+module reg_3 ( input				 Clk, Reset, Load, //make sure to edit for Branch implementation
+				input					[2:0] D, IR,
+				output logic 			[2:0] Data_Out);
+					
+		always_ff @ (posedge Clk)
+		begin
+				// Setting the output Q[16..0] of the register to zeros as Reset is pressed
+				if(Reset) //notice that this is a synchronous reset
+					Data_Out <= 3'b000;
+				// Loading D into register when load button is pressed (will eiher be switches or result of sum)
+				else if(Load)
+					Data_Out <= D;
+		end
+		
+endmodule
+
+module reg_1 ( 	input		Clk, Reset, Load, //for BEN enable
+				input					 D,
+				output logic 		 Data_Out);
+					
+		always_ff @ (posedge Clk)
+		begin
+				// Setting the output Q[7..0] of the register to zeros as Reset is pressed
+				if(Reset) //notice that this is a synchronous reset
+					Data <= 1'b0;
+				// Loading D into register when load button is pressed (will eiher be switches or result of sum)
+				else if(Load)
+					Data <= D;
+				
+	
+		end
+
+endmodule
