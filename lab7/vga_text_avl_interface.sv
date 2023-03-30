bec5c86e52eb82f2b8cbfeecea7b4e99a1f8157b
ArcyCola <arcycruz14@gmail.com>
2023-03-29 23:38:56 -0500

    lord have mercy

--- lab7/vga_text_avl_interface.sv
+++ lab7/vga_text_avl_interface.sv
@@ -59,14 +59,20 @@ logic [31:0] LOCAL_REG       [`NUM_REGS]; // Registers
 
 
 //put other local variables here
-logic [9:0] DrawX, DrawY; 
-
-
+logic [9:0] DrawX, DrawY; 	// position of electron beam on screen
+logic [6:0] charX;			// x coord of the glyph
+logic [5:0] charY;			// y coord of the glyph
+logic [11:0] charAddr;
+logic [9:0] regNum;
+logic [1:0] codeX;
+logic [10:0] addr;
+logic [7:0] codeAddr, data;
+logic blank, pixel_clk;
 //Declare submodules..e.g. VGA controller, ROMS, etc
 
-vga_controller VGA0(.Clk(CLK), .Reset(RESET), .hs, .vs, .pixel_clk(), .blank(), .sync(), .DrawX, .DrawY);
+vga_controller VGA0(.Clk(CLK), .Reset(RESET), .hs, .vs, .pixel_clk, .blank(blank), .sync(), .DrawX, .DrawY);
 
-//font_rom font_rom0();
+font_rom font_rom0(.addr(addr), .data(data));
    
 // Read and write from AVL interface to register block, note that READ waitstate = 1, so this should be in always_ff
 always_ff @(posedge CLK) begin
@@ -111,7 +117,72 @@ end
 
 //handle drawing (may either be combinational or sequential - or both).
 		
-always_comb begin 
+always_comb 
+begin 
+	// 640 x 480 pixels = 80 columns x 30 rows
+	// 0 -> 2400 character addresses
+
+	// finding the character address, find x and y addresses first
+	// charX = drawX / 8	(8 is width of glyph)
+	// charY = drawY / 16	(16 is height of glyph)
+	charX = drawX[9:3]; // divding by 8, because theres 640 / 80 = 8 (this is shifting right 3 times)
+	charY = drawY[9:4]; // divding by 16 cuz 480 / 30 = 16 (this is shifting right 4 times)
+	
+	charAddr = charY * (2'd80) + charX; // 80 is # of columns in a row/line
+
+	// finding the register where character is contained at
+	// 1 register = 4 glyphs
+	regNum = charAddr / (1'd4);		// divide by 4 bc 4 glyphs in 1 register
+	// finding glyph we are at inside the register
+	codeX = charAddr % (1'd4);		// 1 register = [code0][code1][code2][code3]
+	
+	// finding "adder" input for font_rom.sv
+	unique case(codeX) 
+		2'b00 : codeAddr = LOCAL_REG[regNum][31:24];
+		2'b01 : codeAddr = LOCAL_REG[regNum][23:16];
+		2'b10 : codeAddr = LOCAL_REG[regNum][15:8];
+		2'b11 : codeAddr = LOCAL_REG[regNum][7:0];
+	endcase
+	addr = {codeAddr, drawY % 16};
+
+	// check blank (active low)
+	// if blank = 0 show black
+	// if blank = 1 show color and....
+	// check data[7] to see if invert or not
+	// if data[7] = 1 invert fg/bg
+	// if data[7] = 0 default	
+
+// 	Control Register Format:
+//  [[31-25][24-21][20-17][16-13][ 12-9][ 8-5 ][ 4-1 ][   0    ] 
+//  [[RSVD ][FGD_R][FGD_G][FGD_B][BKG_R][BKG_G][BKG_B][RESERVED]
+end
+
+always_ff @(posedge pixel_clk) begin
+	if(blank) begin	// blank = 1 show color
+		if (codeAddr[7] ^ data[drawX%8]) begin
+				red = LOCAL_REG[CTRL_REG][24:21];	// foreground red
+				blue = LOCAL_REG[CTRL_REG][16:13];	// foreground blue
+				green = LOCAL_REG[CTRL_REG][20:17];	// foreground green
+			
+		end
+		else begin	// data[7] = 0 default
+				red =  LOCAL_REG[CTRL_REG][12:9];
+				green = LOCAL_REG[CTRL_REG][8:5];
+				blue = LOCAL_REG[CTRL_REG][4:1];
+		end
+	end
+	else begin	// blank = 0 show black
+		red = 4'b0000;
+		green = 4'b0000;
+		blue = 4'b0000;
+	end
+// 	VRAM Format:
+//  X->
+//  [ 31  30-24][ 23  22-16][ 15  14-8 ][ 7    6-0 ]
+//  [IV3][CODE3][IV2][CODE2][IV1][CODE1][IV0][CODE0]
+
+	//font_rom, lower 4 bits denotes Y coordinate, 
+	//          upper 7 denotes index of glyph
 
 end
 
