5db43be5c52a2643f0219255671d050784b6a5ce
ArcyCola <67534285+ArcyCola@users.noreply.github.com>
2023-05-10 15:38:29 -0500

    random shit

--- final_project/software/usb_kb_bsp/public.mk
+++ final_project/software/usb_kb_bsp/public.mk
@@ -157,9 +157,9 @@ SOPC_SYSID_FLAG += --sidp=0x58
 ELF_PATCH_FLAG  += --sidp 0x58
 
 # The SOPC Timestamp 
-# setting SOPC_TIMESTAMP is 1683318486
-SOPC_SYSID_FLAG += --timestamp=1683318486
-ELF_PATCH_FLAG  += --timestamp 1683318486
+# setting SOPC_TIMESTAMP is 1683322789
+SOPC_SYSID_FLAG += --timestamp=1683322789
+ELF_PATCH_FLAG  += --timestamp 1683322789
 
 # Enable JTAG UART driver to recover when host is inactive causing buffer to 
 # full without returning error. Printf will not fail with this recovery. none 
